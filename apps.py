5834fcefdc02c6498a9cf037d71afb9006b3d359
Yanran Qiu <yanranqius@gmail.com>
2021-01-24 19:41:33 +0800

    modify routes and templates

--- apps.py
+++ apps.py
@@ -27,64 +27,38 @@ class GRNA(db.Model):
 def index():
     return render_template("index.html")
 
+@app.route('/single-dup')
+def single_dup():
+    return render_template("single-dup.html")
 
-@app.route('/single')
-def single():
-    return render_template("single.html")
-
+@app.route('/single-subclass')
+def single_subclass():
+    return render_template("single-subclass.html")
 
 @app.route('/combination')
 def combination():
     return render_template("combination.html")
 
-
-@app.route('/result-single', methods=["POST"])
-def result_single():
-    # if request.method == "POST":
-    item = request.form.get("item")
-    print(item)
-    if item includes 'dup':
-        find = GRNA.query.filter(GRNA.info.contains(item)).limit(5).all()
-        if find:
-            return redirect(url_for("result_single_dup"), =find)
-        else:
-            return render_template("help.html")
-    else:
-        find = GRNA.query.filter(GRNA.info.contains(item)).limit(5).all()
-        if find:
-            return redirect(url_for("result_single_subclass"), =find)
-        else:
-            return render_template("help.html")
-    # else:
-    #     if "result_single" in session:
-    #         return render_template("result-single.html", result=session.get("result_single"))
-    #     else:
-    #         return redirect(url_for("single"))
-
 @app.route('/result-single-dup', methods=["POST"])
-def result_single_dup(find):
-    return render_template("result-single-dup.html")
+def result_single_dup():
+    if result not null:
+        return render_template("result-single-dup.html", result=find)
+    else:
+        return redirect(url_for("help"))
 
 @app.route('/result-single-subclass', methods=["POST"])
 def result_single_subclass():
-    return render_template("result-single-subclass.html")
+    if result not null:
+        return render_template("result-single-subclass.html", result=find)
+    else:
+        return redirect(url_for("help"))
 
 @app.route('/result-combination', methods=["POST"])
 def result_combination():
-    # if request.method == "POST":
-    item = request.form.get("item")
-    print(item)
-    find = GRNA.query.filter(GRNA.info.contains(item)).limit(5).all()
-    session["result_combination"] = find
-    if find:
+    if result not null:
         return render_template("result-combination.html", result=find)
     else:
-        return render_template("help.html")
-    # else:
-    #     if "result_combination" in session:
-    #         return render_template("result-combination.html", result=session.get("result_combination"))
-    #     else:
-    #         return redirect(url_for("combination"))
+        return redirect(url_for("help"))
 
 @app.route('/help')
 def help():
